5a47eab5943b3ea036e3b3deedadeedcb977f4c7
heather frank <sjcswank@gmail.com>
2024-02-09 13:58:56 -0600

    Created test_search.py

---
+++ tests/test_search.py
@@ -0,0 +1,22 @@
+"""
+These tests cover DuckDuckGo searches
+"""
+
+
+def test_basic_duckduckgo_search():
+    # Given the DuckDuckGo homepage is displayed
+    # TODO
+
+    # When the user searches "panda"
+    # TODO
+
+    # Then the search result title contains "panda"
+    # TODO
+
+    # And the search result query is "panda"
+    # TODO
+
+    # And the search result links pertain to "panda"
+    # TODO
+
+    raise Exception("Incomplete Test")
