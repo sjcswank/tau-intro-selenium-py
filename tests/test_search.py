4a604c28585d171f4901fb0105f0599d4a4a082f
heather frank <sjcswank@gmail.com>
2024-02-10 13:06:26 -0600

    Parametrized test

--- tests/test_search.py
+++ tests/test_search.py
@@ -1,28 +1,29 @@
 """
 These tests cover DuckDuckGo searches
 """
+import pytest
 
 from pages.search import DuckDuckGoSearchPage
 from pages.result import DuckDuckGoResultPage
 
-def test_basic_duckduckgo_search(browser):
+
+@pytest.mark.parametrize("phrase", ["panda", "python", "polar bear"])
+def test_basic_duckduckgo_search(browser, phrase):
     search_page = DuckDuckGoSearchPage(browser)
     result_page = DuckDuckGoResultPage(browser)
-    PHRASE = "panda"
 
     # Given the DuckDuckGo homepage is displayed
     search_page.load()
 
-    # When the user searches "panda"
-    search_page.search(PHRASE)
+    # When the user searches phrase
+    search_page.search(phrase)
 
-    # Then the search result title contains "panda"
-    assert PHRASE in result_page.title()
+    # Then the search result query is phrase
+    assert phrase == result_page.search_input_value()
 
-    # And the search result query is "panda"
-    assert PHRASE == result_page.search_input_value()
-
-    # And the search result links pertain to "panda"
+    # And the search result links pertain to phrase
     for title in result_page.result_link_titles():
-        assert PHRASE.lower() in title.lower()
+        assert phrase.lower() in title.lower()
 
+    # And the search result title contains phrase
+    assert phrase in result_page.title()
