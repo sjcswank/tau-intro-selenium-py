a9298eb56e543c034963859e23563c2a826da191
heather frank <sjcswank@gmail.com>
2024-02-11 15:17:27 -0600

    added videos page test

--- pages/videos.py
+++ pages/videos.py
@@ -4,10 +4,31 @@ for the DuckDuckGo videos search page
 """
 
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 
 
 class DuckDuckGoVideosPage:
 
+    URL = "https://duckduckgo.com/?q=panda&t=h_&iar=videos&iax=videos&ia=videos"
+    SEARCH_INPUT = (By.ID, "search_form_input")
+
     def __init__(self, browser):
         self.browser = browser
+
+    def load(self):
+        self.browser.get(self.URL)
+
+    def new_search(self, phrase):
+        search_input = self.browser.find_element(*self.SEARCH_INPUT)
+        search_input.clear()
+        search_input.send_keys(phrase + Keys.RETURN)
+
+    def search_input_value(self):
+        search_input = self.browser.find_element(*self.SEARCH_INPUT)
+        value = search_input.get_attribute("value")
+        return value
+
+    def title(self):
+        return self.browser.title
+
 
--- tests/test_search.py
+++ tests/test_search.py
@@ -6,117 +6,119 @@ import pytest
 from pages.search import DuckDuckGoSearchPage
 from pages.result import DuckDuckGoResultPage
 from pages.images import DuckDuckGoImagesPage
+from pages.videos import  DuckDuckGoVideosPage
 
 
-# @pytest.mark.parametrize("phrase", ["panda", "python", "polar bear"])
-# def test_basic_duckduckgo_search(browser, phrase):
-#     search_page = DuckDuckGoSearchPage(browser)
-#     result_page = DuckDuckGoResultPage(browser)
-#
-#     # Given the DuckDuckGo homepage is displayed
-#     search_page.load()
-#
-#     # When the user searches phrase and hits enter
-#     search_page.enter_search_phrase(phrase)
-#     search_page.send_return_to_search()
-#
-#     # Then the search result query is phrase
-#     assert phrase == result_page.search_input_value()
-#
-#     # And the search result links pertain to phrase
-#     for title in result_page.result_link_titles():
-#         assert phrase.lower() in title.lower()
-#
-#     # And the search result title contains phrase
-#     assert phrase in result_page.title()
-#
-#
-# @pytest.mark.parametrize("phrase", ["panda", "python", "polar bear"])
-# def test_search_by_button_click(browser, phrase):
-#     search_page = DuckDuckGoSearchPage(browser)
-#     result_page = DuckDuckGoResultPage(browser)
-#
-#     # Given the DuckDuckGo homepage is displayed
-#     search_page.load()
-#
-#     # When the user searches phrase and clicks the search button
-#     search_page.enter_search_phrase(phrase)
-#     search_page.click_search_button()
-#
-#     # Then the search result title contains phrase
-#     assert phrase in result_page.title()
-#
-#
-# def test_click_result_link(browser):
-#     result_page = DuckDuckGoResultPage(browser)
-#
-#     # Given the DuckDuckGo result is displayed
-#     result_page.load()
-#
-#     # When the user clicks on the first result
-#     result_page.click_result()
-#
-#     # Then the current url does not contain DuckDuckGo
-#     assert "duckduckgo" not in result_page.current_url()
-#
-#
-# def test_click_more_results(browser):
-#     result_page = DuckDuckGoResultPage(browser)
-#
-#     # Given the DuckDuckGo result page is displayed
-#     result_page.load()
-#
-#     # When the user clicks the more results button
-#     result_page.click_more_results()
-#
-#     # Then there are more than 10 results shown
-#     assert result_page.count_results() > 10
-#
-#
-# def test_autocomplete_contains_phrase(browser):
-#     result_page = DuckDuckGoResultPage(browser)
-#
-#     # Given the DuckDuckGo result page is deisplayed
-#     result_page.load()
-#
-#     # When the user clicks in the search input box
-#     result_page.click_search_input()
-#
-#     # Then the autocomplete suggestions will pertain to the phrase
-#     for option in result_page.get_autocomplete_options():
-#         assert "panda" in option
-#
-#
-# def test_autocomplete_search(browser):
-#     search_page = DuckDuckGoSearchPage(browser)
-#     result_page = DuckDuckGoResultPage(browser)
-#
-#     # Given the DuckDuckGo search page is loaded
-#     search_page.load()
-#
-#     # When the user enters a search term
-#     search_page.enter_search_phrase("p")
-#
-#     # And the user clicks on the first autocomplete suggestion
-#     autocomplete_suggestion = search_page.get_autocomplete_suggestion_text()
-#     search_page.click_autocomplete_suggestion()
-#
-#     # Then the search result title contains autocomplete suggestion
-#     assert autocomplete_suggestion in result_page.title()
-#
-#
-# def test_search_from_result_page(browser):
-#     result_page = DuckDuckGoResultPage(browser)
-#
-#     # Given the result page is loaded
-#     result_page.load()
-#
-#     # When the user searches for a new phrase
-#     result_page.search("python")
-#
-#     # Then the result page title contains the new phrase
-#     assert "python" in result_page.title()
-#
+@pytest.mark.parametrize("phrase", ["panda", "python", "polar bear"])
+def test_basic_duckduckgo_search(browser, phrase):
+    search_page = DuckDuckGoSearchPage(browser)
+    result_page = DuckDuckGoResultPage(browser)
+
+    # Given the DuckDuckGo homepage is displayed
+    search_page.load()
+
+    # When the user searches phrase and hits enter
+    search_page.enter_search_phrase(phrase)
+    search_page.send_return_to_search()
+
+    # Then the search result query is phrase
+    assert phrase == result_page.search_input_value()
+
+    # And the search result links pertain to phrase
+    for title in result_page.result_link_titles():
+        assert phrase.lower() in title.lower()
+
+    # And the search result title contains phrase
+    assert phrase in result_page.title()
+
+
+@pytest.mark.parametrize("phrase", ["panda", "python", "polar bear"])
+def test_search_by_button_click(browser, phrase):
+    search_page = DuckDuckGoSearchPage(browser)
+    result_page = DuckDuckGoResultPage(browser)
+
+    # Given the DuckDuckGo homepage is displayed
+    search_page.load()
+
+    # When the user searches phrase and clicks the search button
+    search_page.enter_search_phrase(phrase)
+    search_page.click_search_button()
+
+    # Then the search result title contains phrase
+    assert phrase in result_page.title()
+
+
+def test_click_result_link(browser):
+    result_page = DuckDuckGoResultPage(browser)
+
+    # Given the DuckDuckGo result is displayed
+    result_page.load()
+
+    # When the user clicks on the first result
+    result_page.click_result()
+
+    # Then the current url does not contain DuckDuckGo
+    assert "duckduckgo" not in result_page.current_url()
+
+
+def test_click_more_results(browser):
+    result_page = DuckDuckGoResultPage(browser)
+
+    # Given the DuckDuckGo result page is displayed
+    result_page.load()
+
+    # When the user clicks the more results button
+    result_page.click_more_results()
+
+    # Then there are more than 10 results shown
+    assert result_page.count_results() > 10
+
+
+def test_autocomplete_contains_phrase(browser):
+    result_page = DuckDuckGoResultPage(browser)
+
+    # Given the DuckDuckGo result page is deisplayed
+    result_page.load()
+
+    # When the user clicks in the search input box
+    result_page.click_search_input()
+
+    # Then the autocomplete suggestions will pertain to the phrase
+    for option in result_page.get_autocomplete_options():
+        assert "panda" in option
+
+
+def test_autocomplete_search(browser):
+    search_page = DuckDuckGoSearchPage(browser)
+    result_page = DuckDuckGoResultPage(browser)
+
+    # Given the DuckDuckGo search page is loaded
+    search_page.load()
+
+    # When the user enters a search term
+    search_page.enter_search_phrase("p")
+
+    # And the user clicks on the first autocomplete suggestion
+    autocomplete_suggestion = search_page.get_autocomplete_suggestion_text()
+    search_page.click_autocomplete_suggestion()
+
+    # Then the search result title contains autocomplete suggestion
+    assert autocomplete_suggestion in result_page.title()
+
+
+def test_search_from_result_page(browser):
+    result_page = DuckDuckGoResultPage(browser)
+
+    # Given the result page is loaded
+    result_page.load()
+
+    # When the user searches for a new phrase
+    result_page.search("python")
+
+    # Then the result page title contains the new phrase
+    assert "python" in result_page.title()
+
+
 def test_image_search(browser):
     image_page = DuckDuckGoImagesPage(browser)
 
@@ -136,14 +138,24 @@ def test_image_search(browser):
     # for title in image_page.image_link_titles():
     #     assert "python" in title.lower()
 
-# def test_videos_search(browser):
+    # TODO Add visual tests to image page?
+
+
+def test_videos_search(browser):
+    videos_page = DuckDuckGoVideosPage(browser)
 
     # Given the video search page is loaded
+    videos_page.load()
 
     # When the user searches for a new phrase
+    videos_page.new_search("python")
 
     # Then the search result query is the new phrase
+    assert "python" == videos_page.search_input_value()
 
     # And the search result title contains the new phrase
+    assert "python" in videos_page.title()
 
     # Then the videos will pertain to the new phrase
+
+    # TODO Add visual tests to videos page?
